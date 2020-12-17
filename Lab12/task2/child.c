f7c3bc288544656d80413e835abb3465e7b353c2
Volodymyr <Zolotukhinvolodymyr@gmail.com>
2020-12-17 13:45:36 +0200

    task2 error

--- Lab12/task2/child.c
+++ Lab12/task2/child.c
@@ -4,8 +4,23 @@
 #include <string.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <signal.h>
+
+
+int flag = 0;
+static void handler(int signo)
+{
+	if (signo == SIGUSR1)
+	{
+		flag = 1;
+	}
+}
 int main(int argc, char const *argv[])
 {
+	if (signal(SIGUSR1,handler)==SIG_ERR)
+	{
+		fprintf(stderr, "%s\n", "signal error");
+	}
 	int fd_p;
 	int fd_ch;
 	size_t size;
@@ -15,24 +30,25 @@ int main(int argc, char const *argv[])
 
 	char buf[256];
 	umask(0);
-
-	if (mknod(name_ch,S_IFIFO|0666,0)<0)
+	while(flag == 0)
 	{
-		fprintf(stderr, "%s\n", "error");
+		pause();
 	}
+
 	if (fd_p=open(name_p,O_RDONLY)<0)
 	{
-		fprintf(stderr, "%s\n","error" );
+		fprintf(stderr, "%s\n","error child: p open" );
 	}
 	
-	size = read(fd_p,buf,256);
-	for (int i = 0; i < strlen(buf); ++i)
+	while(read(fd_p,buf,256)==0);
+
+	for (int i = 0; i < 256; ++i)
 	{
 		buf[i] = toupper(buf[i]);
 	}
 	if (fd_p=open(name_ch,O_WRONLY)<0)
 	{
-		fprintf(stderr, "%s\n","error" );
+		fprintf(stderr, "%s\n","error child ch open" );
 	}
 	size = write(fd_ch,buf,256);
 	close(fd_p);
--- Lab12/task2/main.c
+++ Lab12/task2/main.c
@@ -5,9 +5,17 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <signal.h>
 
 int main(int argc, char const *argv[])
 {
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "%s\n", "error");
+	}
+	int sig = atoi(argv[1]);
+
 	int fd_p;
 	int fd_ch;
 	size_t size;
@@ -17,14 +25,21 @@ int main(int argc, char const *argv[])
 
 	char buf[256];
 	umask(0);
-	scanf("%s",buf);
+	
+	if (mknod(name_ch,S_IFIFO|0666,0)<0)
+	{
+		fprintf(stderr, "%s\n", "error ch");
+	}
 	if (mknod(name_p,S_IFIFO | 0666,0)<0)
 	{
-		fprintf(stderr, "%s\n", "error");
+		fprintf(stderr, "%s\n", "error p");
 	}
+
+	kill(sig,SIGUSR1);
+	scanf("%s",buf);
 	if (fd_p=open(name_p,O_WRONLY)<0)
 	{
-		fprintf(stderr, "%s\n","error" );
+		fprintf(stderr, "%s\n","error p open" );
 	}
 
 	
@@ -32,11 +47,12 @@ int main(int argc, char const *argv[])
 
 	if (fd_ch=open(name_ch,O_RDONLY)<0)
 	{
-		fprintf(stderr, "%s\n", "error");
+		fprintf(stderr, "%s\n", "error ch open");
 	}
 
-	size = read(fd_ch,buf,256);
-	printf("%s\n",buf);
+	while(read(fd_ch,buf,256)==0);
+	
+	printf("\n get : %s\n",buf);
 	close(fd_p);
 	close(fd_ch);
 	return 0;
